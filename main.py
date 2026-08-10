2891f61cff3c5e6d391c4e028c6121856398069b
Namra Ajmal <namraajmal12@gmail.com>
2026-08-10 19:32:20 +0500

    Stage 4: full CRUD

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI, HTTPException
-from fastapi.responses import JSONResponse
+from fastapi.responses import JSONResponse, Response
 
 app = FastAPI()
 
@@ -47,3 +47,32 @@ async def create_task(task: dict):
     }
     taskList.append(new_task)
     return JSONResponse(status_code=201, content=new_task)
+
+
+@app.put("/tasks/{id}")
+async def update_task(id: int, updatedtask: dict):
+    if not updatedtask:
+        return JSONResponse(status_code=400, content={"error": "Empty request"})
+    if "title" not in updatedtask and "done" not in updatedtask:
+        return JSONResponse(status_code=400, content={"error": "Invalid request"})
+    for task in taskList:
+        if task["id"] == id:
+            if "title" in updatedtask:
+                if updatedtask["title"] == "":
+                    return JSONResponse(status_code=400, content="Title can't be empty")
+                task["title"] = updatedtask["title"]
+            if "done" in updatedtask:
+                if not isinstance(updatedtask["done"], bool):
+                    return JSONResponse(status_code=400, content="Invalid request")
+                task["done"] = updatedtask["done"]
+            return JSONResponse(status_code=200, content=task)
+    return JSONResponse(status_code=404, content="Task does not exist")
+
+
+@app.delete("/tasks/{id}")
+async def delete_task(id: int):
+    for task in taskList:
+        if task["id"] == id:
+            taskList.remove(task)
+            return Response(status_code=204)
+    return JSONResponse(status_code=404, content="Task does not exist")
