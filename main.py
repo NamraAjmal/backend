7aad4683bcd69b26bc43ab78e46d51041b61609e
Namra Ajmal <namraajmal12@gmail.com>
2026-08-19 13:31:28 +0500

    full CRUD on Postgres

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from fastapi import FastAPI, HTTPException
 from fastapi.responses import JSONResponse, Response
-import sqlite3
 import psycopg
 import os
 from dotenv import load_dotenv
@@ -57,12 +56,15 @@ async def create_task(task: dict):
     if "title" not in task or task["title"] == "":
         raise HTTPException(status_code=400, detail="Title does not exist")
 
-    connection = sqlite3.connect("tasks.db")
-    cursor = connection.cursor()
-    cursor.execute("INSERT INTO tasks (title,done) VALUES (?,?)", (task["title"], 0))
-    connection.commit()
-    new_task = {"id": cursor.lastrowid, "title": task["title"], "done": False}
-    connection.close()
+    with psycopg.connect(DATABASE_URL) as conn:
+        with conn.cursor() as cur:
+            cur.execute(
+                "INSERT INTO tasks (title,done) VALUES (%s,%s) RETURNING *",
+                (task["title"], False),
+            )
+            row = cur.fetchone()
+            conn.commit()
+    new_task = {"id": row[0], "title": row[1], "done": row[2]}
     return JSONResponse(status_code=201, content=new_task)
 
 
@@ -72,12 +74,12 @@ async def update_task(id: int, updatedtask: dict):
         return JSONResponse(status_code=400, content={"error": "Empty request"})
     if "title" not in updatedtask and "done" not in updatedtask:
         return JSONResponse(status_code=400, content={"error": "Invalid request"})
-    connection = sqlite3.connect("tasks.db")
-    cursor = connection.cursor()
-    cursor.execute("SELECT * FROM tasks where id=?", (id,))
-    row = cursor.fetchone()
+    with psycopg.connect(DATABASE_URL) as conn:
+        with conn.cursor() as cur:
+            cur.execute("SELECT * FROM tasks WHERE id=%s", (id,))
+            row = cur.fetchone()
     if row is None:
-        return JSONResponse(status_code=404, content="Task does not exist")
+        return JSONResponse(status_code=404, content={"error": "Task does not exist"})
     new_title = row[1]
     new_done = row[2]
     if "title" in updatedtask:
@@ -90,26 +92,27 @@ async def update_task(id: int, updatedtask: dict):
                 status_code=400, content="Done can only be a boolean value"
             )
         new_done = updatedtask["done"]
-    cursor.execute(
-        "UPDATE tasks SET title=?, done=? WHERE id=?",
-        (new_title, new_done, id),
-    )
-    connection.commit()
-    connection.close()
-    new_task = {"id": id, "title": new_title, "done": bool(new_done)}
+    with psycopg.connect(DATABASE_URL) as conn:
+        with conn.cursor() as cur:
+            cur.execute(
+                "UPDATE tasks SET title=%s, done=%s WHERE id=%s",
+                (new_title, new_done, id),
+            )
+        conn.commit()
+    new_task = {"id": id, "title": new_title, "done": new_done}
     return JSONResponse(status_code=200, content=new_task)
 
 
 @app.delete("/tasks/{id}", description="Deletes a task")
 async def delete_task(id: int):
-    connection = sqlite3.connect("tasks.db")
-    cursor = connection.cursor()
-    cursor.execute("SELECT * FROM tasks where id=?", (id,))
-    row = cursor.fetchone()
+    with psycopg.connect(DATABASE_URL) as conn:
+        with conn.cursor() as cur:
+            cur.execute("SELECT * FROM tasks WHERE id=%s", (id,))
+            row = cur.fetchone()
     if row is None:
-        connection.close()
         return JSONResponse(status_code=404, content="Task does not exist")
-    cursor.execute("DELETE FROM tasks where id=?", (id,))
-    connection.commit()
-    connection.close()
+    with psycopg.connect(DATABASE_URL) as conn:
+        with conn.cursor() as cur:
+            cur.execute("DELETE FROM tasks where id=%s", (id,))
+        conn.commit()
     return Response(status_code=204)
